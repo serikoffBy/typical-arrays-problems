c6cdd9efc6c3b4f3a344a5699aa347960a1a9f51
serikoffBy <60609970+serikoffBy@users.noreply.github.com>
2020-09-29 00:03:22 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length === 0) {return 0;}
+  else {
+    return Math.min(...array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0) {return 0;}
+  else {
+    return Math.max(...array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length === 0) {return 0;}
+  else {
+    return array.reduce((sum, currentItem) => (sum + currentItem)) / array.length;
+  }
 }
